b8324a7151e6c7ea0b5ce9d027ac4329894482b1
Hossain-A1 <mrhossainahmed7@gmail.com>
2024-08-23 11:02:25 +0300

    added

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  const url = "http://localhost:4000";
+  const url = "https://food-del-api-vlj7.onrender.com";
   const navigate = useNavigate();
   const [food_list, setFoodList] = useState([]);
 
